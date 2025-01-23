3c8308fd25ff0c6bbdfdf8a8ce5d17872365f94b
Durbhe <mcpnsh@gmail.com>
2025-01-23 14:13:22 -0300

    Simples calculadora de nota.

--- src/Arrays/Projeto01/Calculadora.java
+++ src/Arrays/Projeto01/Calculadora.java
@@ -10,15 +10,17 @@ public class Calculadora {
         Scanner scan = new Scanner(System.in);
         DecimalFormat df = new DecimalFormat("#.00");
 
+        System.out.println("Digite o nome do Aluno: ");
+        String nome = scan.next();
+
         System.out.println("Digite a quantidade de notas: ");
         int quantidade = scan.nextInt();
 
         float[] notas = new float[quantidade];
 
         for(int i =0; i<quantidade; i++){
-            System.out.println("Digite a nota "+(i+1)+" :");
+            System.out.println("Digite a "+(i+1)+"ª nota :");
             notas[i] = scan.nextFloat();
-
         }
 
         float soma = 0;
@@ -28,14 +30,14 @@ public class Calculadora {
 
         float media = soma/quantidade;
 
-        System.out.println("Média = " + df.format(media));
+        System.out.println("A média do aluno "+nome+" é: " + df.format(media));
 
         if(media>7){
-            System.out.println("Aprovado");
+            System.out.println("Foi Aprovado");
         }else if(media<=6.9 & media>=5){
-            System.out.println("Recuperação");
+            System.out.println("Está de Recuperação");
         }else{
-            System.out.println("Reprovado");
+            System.out.println("Foi Reprovado");
         }
 
         scan.close();
